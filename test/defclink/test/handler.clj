feafbc685bd6a07b12cf54dd1d34146ffa354fec
Mark Simpson <verdammelt@gmail.com>
2013-07-07 15:18:16 -0400

    Adding an initial test for the nav bar

--- test/defclink/test/handler.clj
+++ test/defclink/test/handler.clj
@@ -12,3 +12,25 @@
              (:body (get-url "/")) => (contains "Welcome to (defclink)!"))
        (fact "not-found route"
              (:status (get-url "/invalid")) => 404))
+
+(defn- find-all-tag [tag html-vec]
+  (let [tags (atom '())]
+    (clojure.walk/postwalk 
+     #(if (and (vector? %)
+               (keyword? (first %))
+               (= (name tag)
+                  (first (clojure.string/split (name (first %)) #"\."))))
+        (swap! tags conj %)
+        %) html-vec)
+    (reverse @tags)))
+
+(defn- get-href-for-anchor [anchor-vec]
+  (filter (complement nil?) (map :href anchor-vec)))
+
+(defn- all-anchor-hrefs [html-vec]
+  (flatten (map get-href-for-anchor (find-all-tag :a html-vec))))
+
+(facts "navbar"
+  (fact "contains link to main page"
+    (all-anchor-hrefs (navbar "/")) => (contains "/")
+    ))
