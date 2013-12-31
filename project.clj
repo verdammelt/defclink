7f01de01e8600ec350cd6540e1aa0c19bd593bed
Mark Simpson <verdammelt@gmail.com>
2013-12-30 20:33:17 -0500

    Updating all dependencies.

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "Clink glasses virtually"
   :url "http://clink.heroku.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.3"]
-                 [ring/ring-jetty-adapter "1.1.0"]]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]
+                 [ring/ring-jetty-adapter "1.2.1"]]
   :min-lein-version "2.0.0"
-  :plugins [[lein-ring "0.8.5"]
+  :plugins [[lein-ring "0.8.8"]
             [ccfontes/lein-cljsbuild "0.3.2-no-exit"]]
   :hooks [leiningen.cljsbuild]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
-                                  [midje "1.5.1"]]}}
+                                  [midje "1.6.0"]]}}
 
   :cljsbuild {
               :builds [{:source-paths ["src-cljs"]
@@ -18,5 +18,5 @@
                                    :optimizations :whitespace
                                    :pretty-print true}}]
               }
-
-    :ring {:handler defclink.handler/app})
+  
+  :ring {:handler defclink.handler/app})
