96088eee5790784db522862c47cfd594e26c3ed0
Mark Simpson <verdammelt@gmail.com>
2013-12-31 08:50:29 -0500

    Updating cljbuild & clojurescript dependencies.

--- project.clj
+++ project.clj
@@ -2,12 +2,13 @@
   :description "Clink glasses virtually"
   :url "http://clink.heroku.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2138"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]
                  [ring/ring-jetty-adapter "1.2.1"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.8.8"]
-            [ccfontes/lein-cljsbuild "0.3.2-no-exit"]]
+            [lein-cljsbuild "1.0.1"]]
   :hooks [leiningen.cljsbuild]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
                                   [midje "1.6.0"]]}}
